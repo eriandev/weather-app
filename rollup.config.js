f2e352b1c2b6c297aca35906db9732cd7f44e14a
erianvc <erianvc.dev@gmail.com>
2020-06-25 17:41:26 -0500

    chore: cambia procesamiento del CSS Se pasa a usar svelte-preprocess para el procesamiento del CSS, permitiendo el uso de preprocesadores como SASS o Styles y el uso de las directivas de TailwindCSS en componentes de Svelte.

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
+import autoPreprocess from 'svelte-preprocess'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -20,9 +21,9 @@ export default {
 			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
-			css: css => {
-				css.write('public/bundle.css', !production);
-			}
+			preprocess: autoPreprocess({
+				postcss: true,
+			}),
 		}),
 
 		// If you have external dependencies installed from
