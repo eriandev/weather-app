1a04479a3d9d11707ac372f8975b15f0cec1e3d9
erianvc <erianvc.dev@gmail.com>
2022-05-21 23:50:07 -0500

    feat: adds container component style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,2 @@
-<main class="text-center mx-auto grid content-center h-screen grid-rows-[fit-content(100%)]">
-  <h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
-  <p>Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</main>
+<h1 class="text-2xl font-bold text-center">Welcome to SvelteKit</h1>
+<p class="text-center">Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
