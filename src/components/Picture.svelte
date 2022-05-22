bc0743d8061cf5794ca1f695a71aa53148a03795
erianvc <erianvc.dev@gmail.com>
2022-05-22 02:19:07 -0500

    feat: adds picture component

---
+++ src/components/Picture.svelte
@@ -0,0 +1,26 @@
+<script>
+  /** @type {string} */
+  export let name
+  /** @type {string} */
+  export let alt
+  /** @type {boolean} */
+  export let animated = false
+</script>
+
+<picture class="filter drop-shadow-2xl">
+  <source srcset="images/webp/{name}.webp" type="image/webp" />
+  <source srcset="images/png/{name}.png" type="image/png" /> 
+  <img class:animated src="images/png/{name}.png" {alt} title={alt} {...$$restProps} on:click />
+</picture>
+
+<style lang="postcss">
+  .animated {
+    animation: float 6s ease-in-out infinite;
+  }
+
+  @keyframes float {
+    0% { transform: translatey(0px) }
+    50% { transform: translatey(-20px) }
+    100% { transform: translatey(0px) }
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
   import Header from '@/components/Header.svelte'
+  import Picture from '@/components/Picture.svelte'
 </script>
 
 <Header title="Kraków" date="September 10, 2021" />
+<Picture animated class="mt-12" name="day-sun" alt="Day Sun" />
