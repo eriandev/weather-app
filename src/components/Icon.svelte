cad99faa59b51656a81f8808903ca5241be6578a
erianvc <erianvc.dev@gmail.com>
2022-05-22 00:32:17 -0500

    feat: adds icon component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,16 @@
+<script>
+  // @ts-ignore
+  import InlineSVG from 'svelte-inline-svg'
+
+  /** @type {string} */
+  export let name
+  /** @type {number} */
+  export let size = 32
+
+  const sizing = {
+    width: size,
+    height: size,
+  }
+</script>
+
+<InlineSVG class="outline-none" src="icons/{name}.svg" {...sizing} {...$$restProps} on:click />
