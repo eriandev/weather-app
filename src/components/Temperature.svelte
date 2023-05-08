d6d433361b52f0c60dee1d89c304fc2298d5a0fe
Erick Vargas <erianvc.dev@gmail.com>
2023-05-07 20:05:26 -0500

    fix: corrects temp number background color

--- src/components/Temperature.svelte
+++ src/components/Temperature.svelte
@@ -6,10 +6,24 @@
 </script>
 
 <section class="font-extrabold">
-  <h2
-    class="bg-clip-text text-[144px] leading-[196px] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent] [background:linear-gradient(180deg,#111827_0%,#6B7280_100%)] dark:[-webkit-background-clip:text] dark:[-webkit-text-fill-color:transparent] dark:[background:linear-gradient(180deg,#F3F4F6_0%,#1F2937_100%)]"
-  >
+  <h2 class="temp text-[144px] leading-[196px]">
     {tempDegrees}°
   </h2>
   <span class="text-{tempCondition} text-2xl capitalize">{tempCondition}</span>
 </section>
+
+<style>
+  h2 {
+    background: linear-gradient(180deg, #111827 0%, #6b7280 100%);
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+  }
+
+  :global(.dark h2.temp) {
+    background: linear-gradient(180deg, #f3f4f6 0%, #1f2937 100%);
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+  }
+</style>
